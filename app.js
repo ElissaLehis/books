1702bab7016d4e5adc92036adf0a6526ce4e6b90
Elissa Lehis <elissa.lehis@khk.ee>
2021-12-07 17:23:10 +0200

    Added function addBook

--- app.js
+++ app.js
@@ -7,11 +7,34 @@ form.addEventListener('submit', addBook)
 
 function addBook(event){
     const title = titleInput.value
-    const author = authorInput.value
-    const isbn = isbnInput.value
+
+    const tr = document.createElement('tr')
+    tr.className ="andmed"
+    const text = document.createTextNode(title)
+    tr.appendChild(text)
+    console.log(tr)
+
     event.preventDefault()
 }
+/*function addBook(event){
+    const author = authorInput.value
+    const tr = document.createElement('tr)
+    tr.className = 'andmed'
+    const text = document.createTextNode(author)
+    tr.appendChild(text)
 
+    event.preventDefault()
+
+}
+function addBook(event){
+    const isbn = isbnInput.value
+    const tr = document.createElement('tr')
+    tr.className = 'andmed'
+    const text = document.createTextNode(isbn)
+    tr.appendChild(text)
+
+    event.preventDefault()
+}*/
 
 
 
